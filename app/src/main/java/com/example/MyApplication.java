f23f4b527b0e7b9cecbe4bde8e8e47b5217d9994
devin naudiyal <naudiyal@gmail.com>
2023-08-02 21:06:15 -0400

    Changes

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -4,5 +4,6 @@ public class MyApplication {
     public static void main(String[] args)
     {
         new PrintService().print(new MessageModel("Hi!! :) :) :) 1 2 3"));
+        new PrintService().print(new MessageModel(":( :( :( 1 2 3"));
     }
 }
