63c42dcb3184d6646a474b96c7bdc449f23d83cb
naudiyal <naudiyal@gmail.com>
2023-08-02 20:55:37 -0400

    Update MyApplication.java

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -3,6 +3,6 @@ package com.example;
 public class MyApplication {
     public static void main(String[] args)
     {
-        new PrintService().print(new MessageModel("Hi!! :) :)"));
+        new PrintService().print(new MessageModel("Hi!! :) :) :)"));
     }
 }
