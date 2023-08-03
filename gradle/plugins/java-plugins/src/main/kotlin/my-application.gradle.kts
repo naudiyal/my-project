c49afed1f31340848971fe423d3334f1cf22aea8
devin naudiyal <naudiyal@gmail.com>
2023-08-03 02:27:20 -0400

    Changes

--- gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
@@ -25,3 +25,14 @@ tasks.register<Zip>("bundle"){
 
     destinationDirectory.set(layout.buildDirectory.dir("distribution"))
 }
+
+tasks.build{
+    dependsOn(tasks.named("bundle"))
+}
+
+tasks.register("buildAll"){
+    description = "Builds even more!"
+
+    dependsOn(tasks.build)
+    dependsOn(tasks.named("countJars"))
+}
