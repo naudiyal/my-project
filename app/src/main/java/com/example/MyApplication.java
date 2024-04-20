ad04101ba64d522b2d5d9a4f4950eacc567c162d
devin naudiyal <naudiyal@gmail.com>
2024-04-20 12:07:42 -0400

    Changes

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -4,15 +4,6 @@ public class MyApplication {
     public static void main(String[] args)
     {
         new PrintService().print(new MessageModel("Hi!! :) :) :) A B C"));
-        
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1 2 3"));
-        new PrintService().print(new MessageModel(":( :( :( 1 2 3"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1 "));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1 "));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 12121212 "));
-
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 333333 "));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 444444 "));
 
     }
     public void TestA() {String s = "sdfsdf";}
