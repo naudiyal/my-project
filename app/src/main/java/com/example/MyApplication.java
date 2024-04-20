93a7d28fdaae7530717e6601fd598cb3a6f9d146
devin naudiyal <naudiyal@gmail.com>
2024-04-20 14:24:46 -0400

    Changes

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -4,6 +4,7 @@ public class MyApplication {
     public static void main(String[] args)
     {
         new PrintService().print(new MessageModel("From Remote !!!!!"));
+        new PrintService().print(new MessageModel("From Local !!!!!"));
     }
     public void TestA() {String s = "sdfsdf";}
     public void TestB() {String s = "sdfsdf";}
