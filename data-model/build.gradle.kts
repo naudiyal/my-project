3d85b642fe860190e211b0783c3b62ec7e5ca218
devin naudiyal <naudiyal@gmail.com>
2024-04-20 10:50:11 -0400

    Changes

--- data-model/build.gradle.kts
+++ data-model/build.gradle.kts
@@ -1,9 +1,22 @@
+
+//In the older version of Gradle plugin block was not needed but is recommended now
+//Plugin gives meaning to component or a sub project
+
+//By dding following we tell Gradle that
+//  -   We want this project to be Java library, has java code, can be built and will be used in another project
+//When you add id("java-library") InteliJ displays folder com/example as com.example
 plugins{
-    //java-library
-    //id("java-library") //applies id("java")
     id("my-java-library")
+
+    //Following provides all Java core functionality like compiling and testing code
+    //id("java")
+    //java-library add more on top of that
+    //id("java-library")
+
 }
 
+//Following is an extension in Gradle and can be added only if id("my-java-library") plugin is added; like above
+//Configure Java version like following to mention which Java version to use for compiling and  testing code
 //following is an extention in Gradle because if you remove plugin id("java-library") above following will give error
 //java{
 //    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,11 @@
+//following is build name, if not set build gets the name of the folder
 rootProject.name = "my-project"
 
+//Components in repository are binary components i.e. already built
+
+//Following is how to inlcude plugins; Plugins extens Gradle itself
 pluginManagement{ //Gradle plugins
+    //Gradle is extensible with Gradle plugins; Following extend Gradle itself
     repositories.gradlePluginPortal()
     //repositories.mavenCentral()
     //repositories.google()
@@ -9,12 +14,20 @@ pluginManagement{ //Gradle plugins
     //    credentials.username = ""
     //    credentials.password = ""
     //}
+
+    //Build components from source if required
+    //In large projects this helps in splitting one software project into multiple builds
+    //Input parameter is to find where to find other project builds you want to include
     includeBuild("gradle/plugins") //build components from source //my own Gradle plugins
 }
 
 dependencyResolutionManagement{
     repositories.mavenCentral()
+
+    //Following is google specific one which provides android libraries
     //repositories.google()
+
+    //following is example of my own repository
     //repositories.maven("https://my.location/repo")
     //{
     //    credentials.username = ""
@@ -24,6 +37,7 @@ dependencyResolutionManagement{
     // includeBuild("../my-other-project") //build components from source
 }
 
+//Following are sub projects
 include("app")//app
 include("business-logic")//business-logic
 include("data-model")//data-model
