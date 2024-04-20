74fa65f3f679c6d0f4bbe46013ece85c1387d70d
naudiyal <naudiyal@gmail.com>
2024-04-20 13:49:23 -0400

    Update MyApplication.java

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -3,19 +3,7 @@ package com.example;
 public class MyApplication {
     public static void main(String[] args)
     {
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-
+        new PrintService().print(new MessageModel("Header !!!!!"));
     }
     public void TestA() {String s = "sdfsdf";}
     public void TestB() {String s = "sdfsdf";}
