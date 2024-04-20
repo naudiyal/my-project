b3a31c7ff8d84b3e0ae89df23de5f0d3100c6d67
devin naudiyal <naudiyal@gmail.com>
2024-04-20 13:33:35 -0400

    Changes

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -4,12 +4,6 @@ public class MyApplication {
     public static void main(String[] args)
     {
         new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-        new PrintService().print(new MessageModel("Hi!! :) :) :) 1234567"));
-
     }
     public void TestA() {String s = "sdfsdf";}
     public void TestB() {String s = "sdfsdf";}
