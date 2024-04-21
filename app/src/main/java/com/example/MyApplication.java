78d52fa3d224722dfde367609c1c1d58e3c804df
devin naudiyal <naudiyal@gmail.com>
2024-04-20 23:04:53 -0400

    Changes

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -3,9 +3,11 @@ package com.example;
 public class MyApplication {
     public static void main(String[] args)
     {
-        new PrintService().print(new MessageModel("From Local !!!!!"));
+        new PrintService().print(new MessageModel("From Local a !!!!!"));
+        new PrintService().print(new MessageModel("From Local A !!!!!"));
     }
     public void TestA() {String s = "sdfsdf";}
     public void TestB() {String s = "sdfsdf";}
     public void TestC() {String s = "sdfsdf1";}
+    public void TestD() {String s = "sdfsdf1";}
 }
