6a1c2f112cde202a0e4ffc0b18eacedede2f9eba
devin naudiyal <naudiyal@gmail.com>
2024-04-20 23:15:40 -0400

    Changes

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -6,6 +6,7 @@ public class MyApplication {
         new PrintService().print(new MessageModel("MessageModel From Remote Master!!!!!"));
         new PrintService().print(new MessageModel1("MessageModel1 From Local Master!!!!!"));
         new PrintService().print(new MessageModel1("MessageModel2 From Local Master!!!!!"));
+        new PrintService().print(new MessageModel1("MessageModel3 From Local Master!!!!!"));
     }
     public void TestA() {String s = "sdfsdf";}
     public void TestB() {String s = "sdfsdf";}
